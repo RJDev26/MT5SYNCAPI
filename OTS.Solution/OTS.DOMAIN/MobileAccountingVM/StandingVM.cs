5ac0572ceb245189a8d512c8c2ba9b81cf87edd1
RJDev26 <68474302+RJDev26@users.noreply.github.com>
2025-09-14 19:56:32 +0530

    Update standing model for new usp_GetStandingByDate result

--- OTS.Solution/OTS.DOMAIN/MobileAccountingVM/StandingVM.cs
+++ OTS.Solution/OTS.DOMAIN/MobileAccountingVM/StandingVM.cs
@@ -4,10 +4,13 @@ namespace OTS.DOMAIN.MobileAccountingVM
 {
     public class StandingVM
     {
-        public DateTime TradeDate { get; set; }
         public long Login { get; set; }
         public string? Symbol { get; set; }
+        public DateTime ExpiryDate { get; set; }
         public decimal BuyQty { get; set; }
         public decimal SellQty { get; set; }
+        public decimal NetQty { get; set; }
+        public decimal BrokerShare { get; set; }
+        public decimal ManagerShare { get; set; }
     }
 }
