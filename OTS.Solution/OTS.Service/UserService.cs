c15e626da8aa814761eb9b02344ba3a8f57c4c59
RJDev26 <68474302+RJDev26@users.noreply.github.com>
2025-10-31 10:18:58 +0530

    Fix ApplicationUser namespace import

--- OTS.Solution/OTS.Service/UserService.cs
+++ OTS.Solution/OTS.Service/UserService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using MobileAccounting.Entities;
 using OTS.DOMAIN.MobileAccountingVM;
 using OTS.Service.Interfaces;
 
